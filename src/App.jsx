3030e46b783d01e36bd61ab50a7127df7486bb31
Yoana <yoanabarbie@icloud.com>
2024-07-21 11:17:18 +0200

    feat: Hero component with images and text info added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import { Navbar } from './components/Navbar/Navbar'
+import { Hero } from './components/Hero/Hero'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,7 @@ function App() {
     <>
       <div>
         <Navbar />
+        <Hero />
       </div>
     </>
   )
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import group from '../../img/Group 77.png'
+import './Hero.css'
+
+export const Hero = () => {
+  return (
+    <section className='hero'>
+        <img src={group} className='hero--photo' />
+        <h1 className='hero--header'>Online experiences</h1>
+        <p className='hero--text'>Join unique interactive activities led by 
+            one-of-a-kind hosts—all without leaving home.</p>
+    </section>
+  )
+}
